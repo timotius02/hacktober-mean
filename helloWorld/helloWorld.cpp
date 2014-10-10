a30d262e26803327b18df5676642652dcc319714
Timotius Sitorus <timsitorus1@gmail.com>
2014-10-09 23:14:40 -0400

    added testing

---
+++ helloWorld/helloWorld.cpp
@@ -0,0 +1,9 @@
+//
+//  helloWorld.cpp
+//  helloWorld
+//
+//  Created by Timotius Sitorus on 10/7/14.
+//
+//
+
+#include "helloWorld.h"
---
+++ helloWorld/helloWorld.h
@@ -0,0 +1,14 @@
+//
+//  helloWorld.h
+//  helloWorld
+//
+//  Created by Timotius Sitorus on 10/7/14.
+//
+//
+
+#ifndef __helloWorld__helloWorld__
+#define __helloWorld__helloWorld__
+
+#include <stdio.h>
+
+#endif /* defined(__helloWorld__helloWorld__) */
